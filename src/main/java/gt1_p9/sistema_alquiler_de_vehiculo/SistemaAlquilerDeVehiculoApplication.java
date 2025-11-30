e60b0f8b679627c47d17c691607c9748668a12e0
YohalmoCas <CS24009@ues.edu.sv>
2025-11-29 23:45:06 -0600

    Establece conexion con base de datos en postgresql

--- src/main/java/gt1_p9/sistema_alquiler_de_vehiculo/SistemaAlquilerDeVehiculoApplication.java
+++ src/main/java/gt1_p9/sistema_alquiler_de_vehiculo/SistemaAlquilerDeVehiculoApplication.java
@@ -2,9 +2,8 @@ package gt1_p9.sistema_alquiler_de_vehiculo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class SistemaAlquilerDeVehiculoApplication {
 
 	public static void main(String[] args) {
